3d45665f6531307b946e6f1418bbdbb3ae4454c8
MudisDev <martin.bibiano4@outlook.com>
2025-03-18 12:44:40 -0600

    Add: ftp configurado y registrar_usuario.php agregado.
    [.vscode\ftp-sync.json]: Add.
    * Establece la conexion con el servidor ftp en el dominio.
    * Se ha realizado con exito la transferencia de registrar_usuario.php de local a remoto.
    
    [Php\registrar_usuario.php]: Add.
    * Permite registrar usuarios en la bd en hostinger.
    * Se ha realizado la insercion recibiendo parametros atravez de url.

---
+++ Php/registrar_usuario.php
@@ -0,0 +1,38 @@
+<?php
+
+// Recibir las variables
+$username = $_GET["username"];
+$nombre = $_GET["nombre"];
+$email = $_GET["email"];
+$password = $_GET["password"];
+$fecha_registro = $_GET["fecha_registro"];
+$telefono = $_GET['telefono'];
+$genero = $_GET['genero'];
+$foto_perfil = $_GET['foto_perfil'];
+
+
+// Credenciales de autentificacion del servidor 
+$server = 'localhost';
+$user = 'u826668871_root2';
+$passwordb = 'Kazooie2518';
+$bdname = 'u826668871_adoptagatitos';
+
+// Conectar a la bd
+$conn = mysqli_connect($server, $user, $passwordb, $bdname);
+if (!$conn) {
+    die('Error al conectarse a la bd');
+}
+
+// Inserción de datos
+$sql = "INSERT INTO Ciudadano (nombre, username, email, password, fecha_registro, telefono, genero, foto_perfil)
+    VALUES ('$nombre', '$username', '$email', '$password', CURDATE(), '$telefono', '$genero', '$foto_perfil')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Se insertó con éxito";
+} else {
+    echo "Error al insertar: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
+?>
