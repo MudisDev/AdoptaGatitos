37f413fd177fdb8c98dd6d46ff632ef0c9a4c289
MudisDev <martin.bibiano4@outlook.com>
2025-03-31 10:47:17 -0600

    Add: Componente reutilizable navbar.html agregado.
    * Components\navbar.html contiene el html reutilizable.
    * Js\navbarScript.js contiene la logica para reutilizar la navbar.

---
+++ Js/navbarScript.js
@@ -0,0 +1,14 @@
+function cargarNavbar() {
+    fetch("Components/navbar.html")  // Obtener el archivo HTML de la navbar
+      .then(response => response.text())  // Convertir la respuesta en texto
+      .then(html => {
+        const contenedor = document.getElementById("navbarContainer");  // Contenedor donde va a insertarse la navbar
+        contenedor.innerHTML = html;  // Insertar el HTML de la navbar
+      })
+      .catch(error => {
+        console.error("Error al cargar la navbar:", error);  // Manejo de errores
+      });
+  }
+  
+  // Llamar a la función cuando el documento esté completamente cargado
+  document.addEventListener("DOMContentLoaded", cargarNavbar);
