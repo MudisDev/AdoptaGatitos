193ff1c957f674c93cf548bb5f7ba2826da11e55
MudisDev <martin.bibiano4@outlook.com>
2025-03-31 10:17:08 -0600

    Add: adoptar.html agregado.
    * Permite extraer los datos de los gatos desde el backend PHP.

--- Php/mostrar_gatos.php
+++ Php/mostrar_gatos.php
@@ -1,4 +1,10 @@
 <?php
+
+// Habilitar CORS para permitir solicitudes de cualquier origen
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
 // Credenciales de autentificacion del servidor 
 $server = 'localhost';
 $user = 'u826668871_root2';
