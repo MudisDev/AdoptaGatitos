d8d4d365629cfc43401c4bdf0e385208f9041814
MudisDev <martin.bibiano4@outlook.com>
2025-04-01 16:45:41 -0600

    Add: registrar_gato.php agregado.
    * En esta pagina se van a poder registrar los gatos.
    * De momento como está en desarrollo no se necesita iniciar sesion pa registrar.

--- Php/registrar_gato.php
+++ Php/registrar_gato.php
@@ -1,5 +1,10 @@
 <?php
 
+// Habilitar CORS para permitir solicitudes de cualquier origen
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
 $nombre = $_GET['nombre'];
 $genero = $_GET['genero'];
 $foto = $_GET['foto'];
