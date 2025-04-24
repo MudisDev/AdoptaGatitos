2acdcb3137dde169bd260ddd2c89b264ac960581
MudisDev <martin.bibiano4@outlook.com>
2025-04-24 11:09:20 -0600

    Add: Mostrar/ocultar contenido cuando se inicia sesion.
    [index.html]: Add.
    * Cuando se inicia sesion oculta el formulario de inicio de sesion y muestra el boton de cerrar sesion.
    
    [Js\authenticatedUser.js]: Add.
    * Verifica si el usuario sea ha logueado.
    
    [registrar_cuenta.html]: Change.
    * Se agrego el script de authenticatedUser.

---
+++ Js/authenticatedUser.js
@@ -0,0 +1,21 @@
+function authenticatedUser() {
+  //window.addEventListener("DOMContentLoaded", () => {
+  const user = JSON.parse(localStorage.getItem("authenticated user"));
+
+  if (user) {
+    // Muestra elementos solo para usuarios logueados
+    //document.getElementById("usuarioNombre").textContent = `Bienvenido, ${usuario.nombre}`;
+    //document.getElementById("contenidoPrivado").style.display = "block";
+    console.log("Hola ", user);
+    console.log("Hola ", user[0].nombre);
+    return true;
+  } else {
+    // Redirige si no hay sesión o es anónimo
+    //window.location.href = "login.html";
+    console.log("Usuario no autentificado Bv");
+    return false;
+  }
+  //});
+}
+
+//authenticatedUser();
