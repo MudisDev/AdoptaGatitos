0f51669319fa1f4c9a148f383336ffe7abb09bf5
MudisDev <martin.bibiano4@outlook.com>
2025-04-28 18:49:22 -0600

    Add: setUser y setAdmin agregados a Auth.
    * Js\Auth.js ha sido refactorizado, en lugar de solo consultar si el usuario esta logueado, puede consultar si usuario y admin estan logueados, cerrar sesion, etc.

--- Js/Auth.js
+++ Js/Auth.js
@@ -1,37 +1,18 @@
-/* export function authenticatedUser() {
-  //window.addEventListener("DOMContentLoaded", () => {
-  const user = JSON.parse(localStorage.getItem("authenticated user"));
-
-  if (user) {
-    // Muestra elementos solo para usuarios logueados
-    //document.getElementById("usuarioNombre").textContent = `Bienvenido, ${usuario.nombre}`;
-    //document.getElementById("contenidoPrivado").style.display = "block";
-    console.log("Hola ", user);
-    console.log("Hola ", user[0].nombre);
-    return true;
-  } else {
-    // Redirige si no hay sesión o es anónimo
-    //window.location.href = "login.html";
-    console.log("Usuario no autentificado Bv");
-    return false;
-  }
-  //});
-}
-
-//authenticatedUser();
- */
 export const Auth = {
   getUser: () => JSON.parse(localStorage.getItem("authenticated user")),
   getAdmin: () => JSON.parse(localStorage.getItem("authenticated admin")),
+  setUser: (user) => localStorage.setItem("authenticated user", JSON.stringify(user)),
+  setAdmin: (admin) => localStorage.setItem("authenticated admin", JSON.stringify(admin)),
   isUserLoggedIn: () => !!localStorage.getItem("authenticated user"),
   isAdminLoggedIn: () => !!localStorage.getItem("authenticated admin"),
   isAnyoneLoggedIn: () => {
-    !!localStorage.getItem("authenticated user") ||
-      !!localStorage.getItem("authenticated admin");
+    return (
+      !!localStorage.getItem("authenticated user") ||
+      !!localStorage.getItem("authenticated admin")
+    );
   },
   logout: () => {
     localStorage.removeItem("authenticated user");
     localStorage.removeItem("authenticated admin");
-
   },
 };
