7d13e8203f7025c09a00a365415cba1c25987156
MudisDev <martin.bibiano4@outlook.com>
2025-05-09 09:21:16 -0600

    Optimize: Se agrego la clase Conexion de NekoPaper.
    * Esta nueva clase tiene metodos genericos que garantizan la reutilizacion de codigo sin duplicidad.

--- Php/clases/Conexion.php
+++ Php/clases/Conexion.php
@@ -1,4 +1,6 @@
 <?php
+require_once  __DIR__ . '/../config/credentials.php';
+
 class Conexion
 {
     private $server;
@@ -11,10 +13,10 @@ class Conexion
 
     public function __construct(/* $server, $user, $passwordb, $bdname */)
     {
-        $this->server = 'localhost';
-        $this->user = 'u826668871_root2';
-        $this->passwordb = 'Kazooie2518';
-        $this->bdname = 'u826668871_adoptagatitos';
+        $this->server = DB_SERVER;
+        $this->user = DB_USER;
+        $this->passwordb = DB_PASSWORD;
+        $this->bdname = DB_NAME;
 
         // Intentar conexión a la BD
         $this->conn = new mysqli($this->server, $this->user, $this->passwordb, $this->bdname);
@@ -38,7 +40,7 @@ class Conexion
         return "Conexión cerrada.";
     }
 
-    public function SetSelect(string $tabla, array $columnas = ['*'], string $condiciones = '')
+    public function SetSelect(string $tabla, array $columnas = ['*'], string $condiciones = '', bool $is_login = false, string $password = '')
     {
         $cols = implode(", ", $columnas);
         $this->sql = "SELECT $cols FROM $tabla";
@@ -47,19 +49,37 @@ class Conexion
             $this->sql .= " WHERE $condiciones";
         }
 
+        $coincidenciaBusqueda = false;
+
         $resultados = [];
         $resultado = $this->conn->query($this->sql);
 
         if ($resultado && $resultado->num_rows > 0) {
             while ($fila = $resultado->fetch_assoc()) {
-                $resultados[] = $fila; // Cada fila es un diccionario (asociativo)
+
+                if ($is_login) {
+                    if ($password == $fila['password']) {
+                        $coincidenciaBusqueda = true;
+                        $resultados[] = $fila; // Cada fila es un diccionario (asociativo)
+                        break;
+                    }
+                } else {
+                    $coincidenciaBusqueda = true;
+                    $resultados[] = $fila; // Cada fila es un diccionario (asociativo)
+                }
+
+
             }
         }
 
-        return $resultados;
+        if ($coincidenciaBusqueda == false) {
+            return ["Error" => "No hubo coincidencias"];
+        } else {
+            return $resultados;
+        }
     }
 
-    public function IniciarSesion(string $tabla, array $columnas = ['*'], $columna_usuario, $username, $password)
+    /* public function IniciarSesion(string $tabla, array $columnas = ['*'], $columna_usuario, $username, $password)
     {
         $cols = implode(", ", $columnas);
         $username = $this->conn->real_escape_string($username); // para prevenir inyecciones básicas
@@ -81,11 +101,12 @@ class Conexion
                 return ["Error" => "Credenciales incorrectas"];
         }
         return $resultados;
-    }
+    } */
 
-    public function SetDelete(string $tabla, string $condiciones, $id)
+    public function SetDelete(string $tabla, string $condiciones/* , $id */)
     {
-        $this->sql = "DELETE FROM $tabla WHERE $condiciones'$id'";
+        /* $this->sql = "DELETE FROM $tabla WHERE $condiciones'$id'"; */
+        $this->sql = "DELETE FROM $tabla WHERE $condiciones";
         $resultado = $this->conn->query($this->sql);
 
         if ($resultado) {
@@ -146,9 +167,30 @@ class Conexion
         }
     }
 
-    public function SetActualizarRelacion($tabla, $id_primario, $id_foraneo, $columna_actualizar, $condiciones)
+    /* public function SetActualizarRelacion($tabla, $id_primario, $id_foraneo, $columna_actualizar, $condiciones)
     {
         $this->sql = "UPDATE $tabla SET $columna_actualizar = '$id_foraneo' WHERE $condiciones '$id_primario'";
+
+        $resultado = $this->conn->query($this->sql);
+
+        if ($resultado) {
+            if ($this->conn->affected_rows > 0) {
+                return ["Success" => "Update exitoso en tabla $tabla."];
+            } else {
+                return ["Warning" => "La consulta se ejecutó, pero no se actualizo ninguna fila en $tabla."];
+            }
+        } else {
+            return [
+                "error" => "Update fallido en tabla $tabla.",
+                "sql" => $this->sql,
+                "mysql_error" => $this->conn->error
+            ];
+        }
+
+    } */
+    public function SetUpdate($tabla, $columnas_actualizar, $condiciones, )
+    {
+        $this->sql = "UPDATE $tabla SET $columnas_actualizar WHERE $condiciones";
 
         $resultado = $this->conn->query($this->sql);
 
