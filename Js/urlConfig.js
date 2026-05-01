fe019c2567f948f0220724abffe85b7897164d2b
MudisDev <martin.bibiano4@outlook.com>
2026-05-01 13:24:43 -0600

    Fix: Se corrigio el frontend ademas de cambios generales.

--- Js/urlConfig.js
+++ Js/urlConfig.js
@@ -8,7 +8,8 @@ export const api_encargado  = `${php_domain}/api/encargado`;
 export const encargado_iniciar_sesion = `${api_encargado}/iniciar_sesion_encargado.php`;
 export const encargado_registrar_gato = `${api_encargado}/registrar_gato.php`;
 
-export const api_gato  = `${php_domain}/api/gato`;
-export const mostrar_gatos = `${api_gato}/mostrar_gatos.php`;
-export const perfil_gato = `${api_gato}/perfil_gato.php`;
+export const api_mascota  = `${php_domain}/api/mascota`;
+export const perfil_mascota = `${api_mascota}/perfil_mascota.php`;
 
+export const api_lista = `${php_domain}/api/lista`;
+export const mostrar_mascotas = `${api_lista}/mostrar_mascotas.php`;
